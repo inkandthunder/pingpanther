a201a0dacd71a4992f9db32a24c4534e12fc4fd6
Scott DeHaan <scottdehaan@gmail.com>
2018-01-12 10:06:50 -0600

    Adding Loopback status to logs

--- pingpanther/Program.cs
+++ pingpanther/Program.cs
@@ -4,20 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using log4net;
+using System.Collections.Specialized;
+using System.Net.NetworkInformation;
+using System.Net;
 
 namespace pingpanther
 {
     class Program
     {
+        public static NameValueCollection AppSettings { get; }
+        public static List<string> ServerList = new List<string> { };
         private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
         static void Main(string[] args)
         {
+            Loopback();
         }
 
-        public static bool PingServer()
+        static void Loopback()
         {
+            using (var ping = new Ping())
+            {
+                var reply = ping.Send(IPAddress.Loopback);
 
+                try
+                {
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        log.Info("Pinging with server: " + reply.Address);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    log.Error(ex);
+                }
+            }
         }
+
+        //public static List<string> GetServers()
+        //{
+        //    public string Hosts = ReadSetting("hosts");
+        //}
+
+        //public static bool PingServer()
+        //{
+
+        //}
     }
 }
