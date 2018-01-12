e7809b8b6469f8cafa109736935b01410912f7a7
Scott DeHaan <scottdehaan@gmail.com>
2018-01-12 09:52:59 -0600

    Initial commit

---
+++ pingpanther/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+
+namespace pingpanther
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+        }
+
+        public static bool PingServer()
+        {
+
+        }
+    }
+}
